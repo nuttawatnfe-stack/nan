df7ba3a91ba9d5281a0b73963e870198a70a1535
nuttawatnfe-stack <nuttawat.nfe@gmail.com>
2026-09-10 15:04:34 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: ''
+    API_URL: 'https://script.google.com/macros/s/AKfycbzZF8hApArMEYAULoabmEyLzRLukxXLjx3vP-it2fmleV5QglDtMJuIereeUN3NEg-a/exec'
   });
 })();
