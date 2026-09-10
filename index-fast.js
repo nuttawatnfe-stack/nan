facb127189a831d919f5f55b5ef94f8dc47fee09
nuttawatnfe-stack <nuttawat.nfe@gmail.com>
2026-09-10 15:05:16 +0700

    Update index-fast.js

--- index-fast.js
+++ index-fast.js
@@ -1513,7 +1513,7 @@ if(document.readyState==='loading')document.addEventListener('DOMContentLoaded',
 
 /* ===== profile-config.js ===== */
 window.STUDENT_PROFILE_WEB_APP_URL =
-  '';
+  'https://script.google.com/macros/s/AKfycbyantkDgS0JdzcNXr1ZE401OIti4zQ_LEIIK_peHQjwHB01zQmDlSS6j9-cQEMtK-w/exec';
 
 ;
 
